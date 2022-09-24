2d634878ea9ddd98032492bc905b3e19cebad8d6
2019065614 <dohan2038@gmail.com>
2022-09-24 15:37:41 +0900

    dohyeong_11005

--- dohyeong/0918부터/5373_큐빙.py
+++
@@ -1,6 +0,0 @@
-cube = [[] for _ in range(6)]
-for _ in range(3):
-    cube[0].append(['r','r','r']); cube[1].append(['y','y','y']); cube[2].append(['w','w','w'])
-    cube[3].append(['g','g','g']); cube[4].append(['b','b','b']); cube[5].append(['o','o','o'])
-
-print(cube)
